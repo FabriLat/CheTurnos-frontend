6e92ea58644e5823fc71c38badc2dc21350f45be
alanmoex <alan.moex@gmail.com>
2024-09-12 16:35:49 -0300

    installed react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import './App.css'
-import Login from './components/login/Login'
-import { AuthenticationContextProvider } from './components/services/authentication/AuthenticationContext'
+import "./App.css";
+import Login from "./components/login/Login";
+import { AuthenticationContextProvider } from "./services/authentication/AuthenticationContext";
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/login", element: <Login /> },    
-  ]);
+  const router = createBrowserRouter([{ path: "/login", element: <Login /> }]);
 
   return (
     <>
-    <AuthenticationContextProvider>
-      <RouterProvider router={router} />
-    </AuthenticationContextProvider>
+      <AuthenticationContextProvider>
+        <RouterProvider router={router} />
+      </AuthenticationContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
