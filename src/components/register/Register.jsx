afbfaea8158f41753c5cc00a2130a2add4be70b1
unknown <FabricioLattante722@gmail.com>
2024-09-12 16:10:48 -0300

    Manejo de errores formulario registro

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,151 +1,168 @@
-import { useState } from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import { useState,useRef } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 
 const RegisterForm = () => {
 
-    function isAlphanumeric(pass) {
-        let hasLetter = false;
-        let hasNumber = false;
-        for (let i = 0; i < pass.length; i++) {
-            const asciiCode = pass.charCodeAt(i);
-    
-            if (asciiCode >= 48 && asciiCode <= 57) { // 0-9
-                hasNumber = true;
-            } else if ((asciiCode >= 65 && asciiCode <= 90) || // A-Z
-                       (asciiCode >= 97 && asciiCode <= 122)) { // a-z
-                hasLetter = true;
-            } else {
-                return false; 
-            }
-        }
-        return hasLetter && hasNumber;
-    }
-    
 
+  const fullNameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passRef = useRef(null);
+  const confirmPassRef = useRef(null);
+
+ 
   const [formData, setFormData] = useState({
-    fullName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
 
   const [errors, setErrors] = useState({
-    fullName: false,
-    email: false,
-    password: false,
-    confirmPassword: false
+    fullName: true,
+    email: true,
+    password: true,
+    confirmPassword: true,
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = (e) => {
+
     e.preventDefault();
 
+    
+
+    const fullName = fullNameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passRef.current.value;
+    const confirmPassword = confirmPassRef.current.value;
+    
+
     let formIsValid = true;
-
-    const newErrors = {
-      fullName: false,
-      email: false,
-      password: false,
-      confirmPassword: false
-    };
-
-    if (!formData.fullName) {
-        setErrors({...formData,
-            fullName : true
-        });
+    if (!fullName) {
+      setErrors({ ...formData, fullName: true });
       formIsValid = false;
+    } else{
+      setErrors({...formData, fullName: false});
     }
 
-    if (!formData.email) {
-        setErrors({...formData,
-            email : true
-        })
+    if (!email) {
+      setErrors({ ...formData, email: true });
       formIsValid = false;
-    } 
-    if (!formData.password || (formData.password.length) < 8 || isAlphanumeric(formData.password) == false){
-        if((isAlphanumeric(formData.password))==false) {alert("La contraseña no es alfanumerica")}
-        setErrors({...formData,
-            password : true
-        })
-      formIsValid = false;
-    }   
-
-    if (formData.password !== formData.confirmPassword) {
-        setErrors({...formData,
-            confirmPassword : true
-        })
-      formIsValid = false;
+    }else{
+      setErrors({...formData, email: false});
     }
+    if (!password ||password.length < 8 ||isAlphanumeric(password) == false) {
+      setErrors({ ...formData, password: true });
+      formIsValid = false;
+    }else{
+      setErrors({...formData, password: false});
+    }
+ 
 
-    setErrors(newErrors);
+    if (password !== confirmPassword) {
+      setErrors({ ...formData, confirmPassword: true });
+      formIsValid = false;
+    }else{
+      setErrors({...formData, confirmPassword: false});
+    }
+   
 
+    console.log(formIsValid)
     if (formIsValid) {
-      console.log('Formulario enviado', formData);
+      console.log("Formulario enviado", formData);
     }
   };
 
   return (
     <Container className="mt-4">
       <h2>Registro</h2>
-      <Form className='mt-5' onSubmit={handleSubmit}>
+      <Form className="mt-5" onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>Nombre Completo</Form.Label>
           <Form.Control
+            ref={fullNameRef}
             type="text"
             placeholder="Introduce tu nombre completo"
             name="fullName"
             value={formData.fullName}
             onChange={handleChange}
           />
+          {errors.fullName ? "" : "Completa el campo."}
+          
         </Form.Group>
 
-        <Form.Group className='mt-4' >
+        <Form.Group className="mt-4">
           <Form.Label>Correo Electrónico</Form.Label>
           <Form.Control
+            ref={emailRef}
             type="email"
             placeholder="Introduce tu correo electrónico"
             name="email"
             value={formData.email}
             onChange={handleChange}
           />
+          {errors.email ? "" : "Completa el campo."}
         </Form.Group>
 
-        <Form.Group className='mt-4'>
+        <Form.Group className="mt-4">
           <Form.Label>Contraseña</Form.Label>
           <Form.Control
+            ref={passRef}
             type="password"
             placeholder="Introduce tu contraseña"
             name="password"
             value={formData.password}
             onChange={handleChange}
-            isInvalid={!!errors.password}
           />
+          {errors.password ? "" : "Completa el campo."}
         </Form.Group>
 
-        <Form.Group className='mt-4'>
+        <Form.Group className="mt-4">
           <Form.Label>Confirmar Contraseña</Form.Label>
           <Form.Control
+            ref={confirmPassRef}
             type="password"
             placeholder="Confirma tu contraseña"
             name="confirmPassword"
             value={formData.confirmPassword}
             onChange={handleChange}
           />
+          {errors.confirmPassword ? "Las claves no coinciden." : ""}
         </Form.Group>
 
-        <Button className='mt-3' variant="primary" type="submit">
+        <Button className="mt-3" variant="primary" type="submit">
           Registrarse
         </Button>
       </Form>
     </Container>
   );
 };
+
+function isAlphanumeric(pass) {  //Recibe un string y devuelve false si no es alfanumerico =)
+  let hasLetter = false;
+  let hasNumber = false;
+  for (let i = 0; i < pass.length; i++) {
+    const asciiCode = pass.charCodeAt(i);
+    if (asciiCode >= 48 && asciiCode <= 57) { // 0 - 9
+      hasNumber = true;
+    } else if (
+      (asciiCode >= 65 && asciiCode <= 90) ||  // a - z
+      (asciiCode >= 97 && asciiCode <= 122)  // A - Z
+    ) {
+      hasLetter = true;
+    } else {
+      return false;
+    }
+  }
+  return hasLetter && hasNumber;
+}
+
 
 export default RegisterForm;
