3dc841ea53d82885b4f2e3c8ede87a2b807abc8a
unknown <FabricioLattante722@gmail.com>
2024-09-12 20:31:53 -0300

    Interfaz de registro con manejo de errores. Estilo pendiente

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css'
-import Register from './components/register/Register'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import { AuthenticationContextProvider } from "./services/authentication/AuthenticationContext";
 
 function App() {
-
+  const router = createBrowserRouter([
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+  ]);
 
   return (
     <>
-      <Register/>
+      <AuthenticationContextProvider>
+        <RouterProvider router={router} />
+      </AuthenticationContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
