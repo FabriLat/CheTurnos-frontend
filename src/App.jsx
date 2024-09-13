98e92bd2eb3e396a0632d887608496f5f7ce6898
unknown <FabricioLattante722@gmail.com>
2024-09-12 21:04:58 -0300

    ruta register agregada

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login/Login";
 import { AuthenticationContextProvider } from "./services/authentication/AuthenticationContext";
+import Register from "./components/register/Register";
 
 function App() {
-  const router = createBrowserRouter([{ path: "/login", element: <Login /> }]);
+  const router = createBrowserRouter([{ path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> }
+  ]);
 
   return (
     <>
