1cf41f6f4be115a6598ee8cfb553d78db3a22b9f
N-Rem <nicolasromero.barrios@gmail.com>
2024-10-27 13:44:52 -0300

    update screen users

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,64 +1,110 @@
 import React, { useEffect, useState } from 'react'
 import { ListGroup } from 'react-bootstrap'
 import EachUser from './EachUser';
+import Spiner from '../spiner/Spiner';
+import './users.css';
+
 const Users = () => {
+    const [user, setUsers] = useState([]);
 
     const [clients, setClients] = useState([]);
     const [Owners, setOwners] = useState([]);
     const [Employees, setEmployees] = useState([]);
-
+    const [Admins, setAdmins] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        //llamada a la api para conseguir los usuarios
-        fetchClient();
-        fetchOwner();
-        fetchEmployee();
+        //Conseguir los usuarios
+        fetchUser();
     }, []);
 
-    const fetchClient = async () => {
+    //Se actualizan los Usuarios
+    useEffect(() => {
+        setClients(user.filter(u => u.type === "Client"));
+        setOwners(user.filter(u => u.type === "Owner"));
+        setEmployees(user.filter(u => u.type === "Employee"));
+        setAdmins(user.filter(u => u.type === "SysAdmin"));
+    }, [user])
 
-    }
-
-
-    const fetchOwner = async () => {
-
-    }
-
-    const fetchEmployee = async () => {
+    const fetchUser = async () => {
+        try {
+            const response = await fetch(`https://localhost:7276/api/SysAdmin/GetAll`,
+                {
+                    method: "GET",
+                    mode: "cors",
+                }
+            );
+            if (!response.ok) {
+                throw new Error("Error in obtaining Users");
+            }
+            const data = await response.json();
+            setUsers(data);
+            setLoading(false);
+        }
+        catch (error) {
+            console.error("Error:", error)
+        }
 
     }
 
 
     const onDeleteUser = async (userId) => {
-
+        try {
+            const response = await fetch(`https://localhost:7276/api/SysAdmin/Delete/${userId}`,
+                {
+                    method: "DELETE",
+                    mode: "cors",
+                }
+            );
+            if (!response.ok) {
+                throw new Error("Error in deleting User");
+            }
+            setUsers((prevUsers) => prevUsers.filter((u) => u.id !== userId));
+            setLoading(false);
+        }
+        catch (error) {
+            console.error("Error:", error)
+        }
     }
 
     return (
-        <div>
-            <div>
-                <h4>Clients</h4>
-                <ListGroup>
-                    {clients.map((user) => (
-                        <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
-                    ))}
-                </ListGroup>
-            </div>
-            <div>
-                <h4>Dueños</h4>
-                <ListGroup>
-                    {Owners.map((user) => (
-                        <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
-                    ))}
-                </ListGroup>
-            </div>
-            <div>
-                <h4>Empleados</h4>
-                <ListGroup>
-                    {Employees.map((user) => (
-                        <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
-                    ))}
-                </ListGroup>
-            </div>
+        <div >
+            {loading ? (
+                <Spiner />
+            ) : (<div className='container'>
+                <div>
+                    <h4>Clientes</h4>
+                    <ListGroup>
+                        {clients.map((user) => (
+                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
+                        ))}
+                    </ListGroup>
+                </div>
+                <div>
+                    <h4>Dueños</h4>
+                    <ListGroup>
+                        {Owners.map((user) => (
+                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
+                        ))}
+                    </ListGroup>
+                </div>
+                <div>
+                    <h4>Empleados</h4>
+                    <ListGroup>
+                        {Employees.map((user) => (
+                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
+                        ))}
+                    </ListGroup>
+                </div>
+                <div>
+                    <h4>Admins</h4>
+                    <ListGroup>
+                        {Admins.map((user) => (
+                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
+                        ))}
+                    </ListGroup>
+                </div>
+            </div>)}
         </div>
 
     )
