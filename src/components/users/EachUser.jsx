cf1e1a4f07098eadd75310ea356875a125d692fd
Valen <valentinakeila12@gmail.com>
2024-10-29 18:55:50 -0700

    Added Styles on sysAdmin page

--- src/components/users/EachUser.jsx
+++ src/components/users/EachUser.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { Button, ListGroup } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 const EachUser = ({ id, name, email, onDelete }) => {
   return (
-        <ListGroup.Item variant='dark' className="d-flex align-items-center justify-content-between">
+        <ListGroup.Item style={{backgroundColor: '#b0b5d6'}} className="d-flex align-items-center justify-content-between">
             {name}<br/> {email}
             <Button variant='danger' onClick={() => onDelete(id)} >Borrado Permanente</Button>
         </ListGroup.Item>
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,113 +1,156 @@
-import React, { useEffect, useState } from 'react'
-import { ListGroup } from 'react-bootstrap'
+import { useEffect, useState } from 'react';
+import { ListGroup, Form, Modal, Button } from 'react-bootstrap';
 import EachUser from './EachUser';
 import Spiner from '../spiner/Spiner';
 import './users.css';
 
 const Users = () => {
-    const [user, setUsers] = useState([]);
+  const [user, setUsers] = useState([]);
+  const [clients, setClients] = useState([]);
+  const [owners, setOwners] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [admins, setAdmins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedType, setSelectedType] = useState("Client");
+  const [showModal, setShowModal] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState(null);
 
-    const [clients, setClients] = useState([]);
-    const [Owners, setOwners] = useState([]);
-    const [Employees, setEmployees] = useState([]);
-    const [Admins, setAdmins] = useState([]);
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-    useEffect(() => {
-        //Conseguir los usuarios
-        fetchUser();
-    }, []);
+  useEffect(() => {
+    setClients(user.filter(u => u.type === "Client"));
+    setOwners(user.filter(u => u.type === "Owner"));
+    setEmployees(user.filter(u => u.type === "Employee"));
+    setAdmins(user.filter(u => u.type === "SysAdmin"));
+  }, [user]);
 
-    //Se actualizan los Usuarios
-    useEffect(() => {
-        setClients(user.filter(u => u.type === "Client"));
-        setOwners(user.filter(u => u.type === "Owner"));
-        setEmployees(user.filter(u => u.type === "Employee"));
-        setAdmins(user.filter(u => u.type === "SysAdmin"));
-    }, [user])
-
-    const fetchUser = async () => {
-        try {
-            const response = await fetch(`https://localhost:7276/api/SysAdmin/GetAll`,
-                {
-                    method: "GET",
-                    mode: "cors",
-                }
-            );
-            if (!response.ok) {
-                throw new Error("Error in obtaining Users");
-            }
-            const data = await response.json();
-            setUsers(data);
-            setLoading(false);
-        }
-        catch (error) {
-            console.error("Error:", error)
-        }
-
+  const fetchUser = async () => {
+    try {
+      const response = await fetch(`https://localhost:7276/api/SysAdmin/GetAll`, {
+        method: "GET",
+        mode: "cors",
+      });
+      if (!response.ok) {
+        throw new Error("Error in obtaining Users");
+      }
+      const data = await response.json();
+      setUsers(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error:", error);
     }
+  };
 
+  const confirmDeleteUser = (userId) => {
+    setUserIdToDelete(userId);
+    setShowModal(true);
+  };
 
-    const onDeleteUser = async (userId) => {
-        try {
-            const response = await fetch(`https://localhost:7276/api/SysAdmin/Delete/${userId}`,
-                {
-                    method: "DELETE",
-                    mode: "cors",
-                }
-            );
-            if (!response.ok) {
-                throw new Error("Error in deleting User");
-            }
-            setUsers((prevUsers) => prevUsers.filter((u) => u.id !== userId));
-            setLoading(false);
+  const handleDeleteUser = async () => {
+    if (userIdToDelete) {
+      try {
+        const response = await fetch(`https://localhost:7276/api/SysAdmin/Delete/${userIdToDelete}`, {
+          method: "DELETE",
+          mode: "cors",
+        });
+        if (!response.ok) {
+          throw new Error("Error in deleting User");
         }
-        catch (error) {
-            console.error("Error:", error)
-        }
+        setUsers((prevUsers) => prevUsers.filter((u) => u.id !== userIdToDelete));
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+        setShowModal(false);
+        setUserIdToDelete(null);
+      }
     }
+  };
 
-    return (
-        <div >
-            {loading ? (
-                <Spiner />
-            ) : (<div className='container'>
-                <div>
-                    <h4>Clientes</h4>
-                    <ListGroup>
-                        {clients.map((user) => (
-                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
-                        ))}
-                    </ListGroup>
-                </div>
-                <div>
-                    <h4>Dueños</h4>
-                    <ListGroup>
-                        {Owners.map((user) => (
-                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
-                        ))}
-                    </ListGroup>
-                </div>
-                <div>
-                    <h4>Empleados</h4>
-                    <ListGroup>
-                        {Employees.map((user) => (
-                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
-                        ))}
-                    </ListGroup>
-                </div>
-                <div>
-                    <h4>Admins</h4>
-                    <ListGroup>
-                        {Admins.map((user) => (
-                            <EachUser key={user.id} name={user.userName} email={user.email} onDelete={onDeleteUser} id={user.id} />
-                        ))}
-                    </ListGroup>
-                </div>
-            </div>)}
+  const renderUserList = () => {
+    switch (selectedType) {
+      case "Client":
+        return clients;
+      case "Owner":
+        return owners;
+      case "Employee":
+        return employees;
+      case "SysAdmin":
+        return admins;
+      default:
+        return [];
+    }
+  };
+
+  const getUserTypeTitle = () => {
+    switch (selectedType) {
+      case "Client":
+        return "Clientes";
+      case "Owner":
+        return "Dueños";
+      case "Employee":
+        return "Empleados";
+      case "SysAdmin":
+        return "Admins";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div className='outer-container-users'>
+      {loading ? (
+        <Spiner />
+      ) : (
+        <div className="user-container">
+          <Form.Group controlId="userTypeSelect" className="user-select">
+            <Form.Label>Selecciona Tipo de Usuario:</Form.Label>
+            <Form.Select
+              as="select"
+              value={selectedType}
+              onChange={(e) => setSelectedType(e.target.value)}
+            >
+              <option value="Client">Clientes</option>
+              <option value="Owner">Dueños</option>
+              <option value="Employee">Empleados</option>
+              <option value="SysAdmin">Admins</option>
+            </Form.Select>
+          </Form.Group>
+
+          <h4>{getUserTypeTitle()}</h4>
+
+          <ListGroup className="user-list">
+            {renderUserList().map((user) => (
+              <EachUser
+                key={user.id}
+                name={user.userName}
+                email={user.email}
+                onDelete={() => confirmDeleteUser(user.id)}
+                id={user.id}
+              />
+            ))}
+          </ListGroup>
+
+         
+          <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Confirmar Eliminación</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>¿Estás seguro de que deseas eliminar este usuario?</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => setShowModal(false)}>
+                Cancelar
+              </Button>
+              <Button variant="danger" onClick={handleDeleteUser}>
+                Eliminar
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
+      )}
+    </div>
+  );
+};
 
-    )
-}
-
-export default Users
+export default Users;
