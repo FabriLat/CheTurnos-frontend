e29f5339a5b1f6041b6c7f829131e9c0abe04af2
Valen <valentinakeila12@gmail.com>
2024-11-05 20:03:59 -0800

    Adding calendars

--- src/components/clientsAppointmentList/ClientsAppointmentList.jsx
+++ src/components/clientsAppointmentList/ClientsAppointmentList.jsx
@@ -1,17 +1,19 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react';
 import { AuthenticationContext } from '../../services/authentication/AuthenticationContext';
 import ClientsAppointmentItem from '../clientsAppointmentItem/ClientsAppointmentItem';
 import Spiner from '../spiner/Spiner';
 import useValidateUser from '../hookCustom/useValidateUser';
-import './clientAppointments.css'
-
+import './clientAppointments.css';
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const ClientsAppointmentList = () => {
     const { user } = useContext(AuthenticationContext);
     const [loading, setLoading] = useState(true);
     const [appointments, setAppointments] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const { isClient, isEmployee } = useValidateUser();
-
+    const [filterType, setFilterType] = useState("ACTIVO"); 
 
     const fetchClientAppointments = async () => {
         try {
@@ -20,15 +22,14 @@ const ClientsAppointmentList = () => {
                 mode: "cors",
             });
             if (!response.ok) {
-                throw new Error("Error in obtaining appointments")
+                throw new Error("Error en la obtención de turnos");
             }
             const appointmentsData = await response.json();
             setAppointments(appointmentsData);
-            setLoading(false); // desactiva el spiners
-        }
-        catch (error) {
-            console.error("Error:", error)
-            setLoading(false); // desactiva el spiners
+            setLoading(false);
+        } catch (error) {
+            console.error("Error:", error);
+            setLoading(false);
         }
     };
 
@@ -39,27 +40,68 @@ const ClientsAppointmentList = () => {
                 mode: "cors",
             });
             if (!response.ok) {
-                throw new Error("Error in obtaining appointments")
+                throw new Error("Error en la obtención de turnos");
             }
             const appointmentsData = await response.json();
-            setAppointments(appointmentsData);
-            setLoading(false); // desactiva el spiners
-        }
-        catch (error) {
-            console.error("Error:", error)
-            setLoading(false); // desactiva el spiners
+
+            
+            const filteredAppointments = appointmentsData.filter((appointment) => {
+                const isInactive = !appointment.clientName && !appointment.serviceName;
+                const isActive = appointment.clientName || appointment.serviceName;
+                return filterType === "ACTIVO" ? isActive : isInactive;
+            });
+
+            setAppointments(filteredAppointments);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error:", error);
+            setLoading(false);
         }
     };
 
     useEffect(() => {
-        (isEmployee()) && fetchEmployeeAppointments();
-        (isClient()) && fetchClientAppointments();
-    }, [user.id]);
+        if (isClient()) {
+            fetchClientAppointments();
+        } else if (isEmployee()) {
+            fetchEmployeeAppointments();
+        }
+    }, [user.id, filterType]);
+
+    const handleFilterChange = (e) => {
+        setFilterType(e.target.value);
+    };
+
+    const availableDates = new Set(
+        appointments.map((appointment) =>
+            new Date(appointment.dateAndHour).toDateString()
+        )
+    );
+
+    const tileDisabled = ({ date }) => !availableDates.has(date.toDateString());
+
+    const filteredAppointments = appointments.filter((appointment) => {
+        const appointmentDate = new Date(appointment.dateAndHour);
+        return (
+            appointmentDate.getFullYear() === selectedDate.getFullYear() &&
+            appointmentDate.getMonth() === selectedDate.getMonth() &&
+            appointmentDate.getDate() === selectedDate.getDate()
+        );
+    });
+
+    const capitalizeFirstLetter = (string) =>
+        string.charAt(0).toUpperCase() + string.slice(1);
+
+    const formattedDay = capitalizeFirstLetter(
+        selectedDate.toLocaleString("es-AR", { weekday: "long" })
+    );
+    const formattedMonth = capitalizeFirstLetter(
+        selectedDate.toLocaleString("es-AR", { month: "long" })
+    );
+    const formattedDate = `Fecha: ${formattedDay} ${selectedDate.getDate()} de ${formattedMonth} de ${selectedDate.getFullYear()}`;
 
     const removeAppointment = (id) => {
         setAppointments((prevAppointments) => prevAppointments.filter((a) => a.id !== id));
-    }
-
+    };
 
     return (
         <div>
@@ -69,20 +111,54 @@ const ClientsAppointmentList = () => {
                 <div className="outer-container-appointment">
                     <div className="appointment-list-container">
                         <div className="title-service">
-                            <h1 className="service-title">Mis turnos</h1>
+                            <h1 className="service-title">Mis turnos:</h1>
                         </div>
+                        
+                        
+                        {isEmployee() && (
+                            <div className="filter-select" style={{width:'20%', marginLeft:'40%', marginBottom:'1%'}}>
+                                <label htmlFor="filterType" style={{fontSize:'20px'}}>Filtrar turnos:</label>
+                                <select id="filterType" value={filterType} onChange={handleFilterChange}>
+                                    <option value="ACTIVO">Activo</option>
+                                    <option value="INACTIVO">Inactivo</option>
+                                </select>
+                            </div>
+                        )}
+
+                        <div className="calendar-container">
+                            <Calendar
+                                onChange={setSelectedDate}
+                                value={selectedDate}
+                                minDate={new Date()}
+                                tileDisabled={tileDisabled}
+                                tileClassName={({ date }) =>
+                                    availableDates.has(date.toDateString()) ? 'available-date' : null
+                                }
+                            />
+                        </div>
+
+                        <div className="date-div">
+                            <h2 className="date-title">
+                                {formattedDate}
+                            </h2>
+                        </div>
+
                         <div className="card-service">
-                            {appointments.map(a => (
-                                <ClientsAppointmentItem
-                                    key={a.Id}
-                                    shopName={a.shopName}
-                                    serviceName={a.serviceName}
-                                    dateAndHour={a.dateAndHour}
-                                    onRemoveAppointment={removeAppointment}
-                                    clientName={a.clientName}
-                                    id={a.id}
-                                />
-                            ))}
+                            {filteredAppointments.length > 0 ? (
+                                filteredAppointments.map((a) => (
+                                    <ClientsAppointmentItem
+                                        key={a.id}
+                                        shopName={a.shopName}
+                                        serviceName={a.serviceName}
+                                        dateAndHour={a.dateAndHour}
+                                        onRemoveAppointment={removeAppointment}
+                                        clientName={a.clientName}
+                                        id={a.id}
+                                    />
+                                ))
+                            ) : (
+                                <p>No hay turnos disponibles para esta fecha.</p>
+                            )}
                         </div>
                     </div>
                 </div>
